220bdba001015e4aa444194cd6de18d2dd997bc1
Adrien Foulon <6115458+Tofandel@users.noreply.github.com>
2021-12-18 12:01:15 +0100

    Load styles and scripts in parallel
    Instead of loading the style after the script sequentially, load them in parallel for a faster loading

--- src/payline-helper.js
+++ src/payline-helper.js
@@ -27,10 +27,14 @@ function loadStyle(url) {
 
 export const loadPayline = async (isHomologation = false) => {
   if(isHomologation){
-    await loadScript("https://homologation-payment.cdn.payline.com/scripts/widget-min.js");
-    await loadStyle("https://homologation-payment.cdn.payline.com/styles/widget-min.css");
+    await Promise.all([
+      loadScript("https://homologation-payment.cdn.payline.com/scripts/widget-min.js",
+      loadStyle("https://homologation-payment.cdn.payline.com/styles/widget-min.css"),
+    ]);
   } else {
-    await loadScript("https://payment.payline.com/scripts/widget-min.js");
-    await loadStyle("https://payment.payline.com/styles/widget-min.css");
+    await Promise.all([
+      loadScript("https://payment.payline.com/scripts/widget-min.js"),
+      loadStyle("https://payment.payline.com/styles/widget-min.css"),
+    ]);
   }
 };
